4fa079e2681adeab5e67b727147f0137c060cfa8
jja04041 <96465763+jja04041@users.noreply.github.com>
2022-01-09 22:21:13 +0900

    0109-2

--- app/src/main/java/com/example/bookworm/MainActivity.java
+++ app/src/main/java/com/example/bookworm/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.bookworm;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.tabs.TabLayout;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private FragmentPagerAdapter fragmentPagerAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = findViewById(R.id.viewPager);
+        fragmentPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom);
+        viewPager.setAdapter(fragmentPagerAdapter);
+        TabLayout.setupWithViewPager(viewPager);
     }
 }
---
+++ app/src/main/java/com/example/bookworm/fragment_search.java
@@ -0,0 +1,25 @@
+package com.example.bookworm;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class fragment_search extends Fragment {
+
+
+    public static fragment_search newInstance(){
+        fragment_search fragment_search = new fragment_search();
+        return fragment_search;
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+}
