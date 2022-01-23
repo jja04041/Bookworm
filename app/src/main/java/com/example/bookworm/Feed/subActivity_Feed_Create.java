74b9587184c10cdb232c0fdcfca03d4947785fd6
lmy6268 <lmy6268@gmail.com>
2022-01-24 01:00:11 +0900

    피드 작성화면 구성
    아직은 제대로 동작하진 않음.

--- app/src/main/java/com/example/bookworm/Feed/subActivity_Feed_Create.java
+++ app/src/main/java/com/example/bookworm/Feed/subActivity_Feed_Create.java
@@ -3,14 +3,70 @@ package com.example.bookworm.Feed;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import com.example.bookworm.R;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class subActivity_Feed_Create extends AppCompatActivity {
+    EditText edtFeedContent;
+    Button btnAdd;
+    LinearLayout layout;
+    ArrayList<Button> btn;
+    int label = 0;
+    //라벨은 알럿 다이어그램을 통해 입력을 받고, 선택한 값으로 라벨이 지정됨
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.subactivity_feed_create);
+        edtFeedContent = findViewById(R.id.edtFeedContent);
+        btnAdd = findViewById(R.id.btnAdd);
+        layout = findViewById(R.id.llLabel);
+        btn = new ArrayList<>();
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (label < 3) {
+                    Button btn = new Button(getApplicationContext());
+                    btn.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                    btn.setBackground(getDrawable(R.drawable.label_design));
+                    btn.setText("안녕");
+                    Log.d("label", label + "개");
+                    btn.setEnabled(false);
+                    AddBtn(btn);
+                    if (label == 2) {
+                        btnAdd.setVisibility(View.INVISIBLE);
+                    }
+                    label++;
+                }
+            }
+        });
+
+
+    }
+
+    public void AddBtn(Button button) {
+        int count = layout.getChildCount(); //현재 버튼 개수
+        btn.add(button);
+        for (int i = 0; i < count; i++) {
+            btn.add((Button) layout.getChildAt(i));
+        }
+        layout.removeAllViews();
+        for (Button i : btn) {
+            layout.addView(i);
+        }
+        btn.clear();
+    }
+
+    public void RemoveBtn(int idx) {
     }
 }
--- app/src/main/java/com/example/bookworm/fragments/fragment_feed.java
+++ app/src/main/java/com/example/bookworm/fragments/fragment_feed.java
@@ -1,5 +1,6 @@
 package com.example.bookworm.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,9 +11,10 @@ import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
 import com.example.bookworm.R;
+import com.example.bookworm.Feed.subActivity_Feed_Create;
 
 public class fragment_feed extends Fragment {
-
+    ImageView imgCreate;
 
 
     @Override
@@ -24,10 +26,21 @@ public class fragment_feed extends Fragment {
         ImageView profileimg = (ImageView) view.findViewById(R.id.img_profile);
         ImageView profileimg_like = (ImageView) view.findViewById(R.id.img_like_profile);
         ImageView profileimg_reply = (ImageView) view.findViewById(R.id.img_reply_profle);
+        imgCreate=(ImageView)view.findViewById(R.id.img_createfeed);
 
         Glide.with(this).load(R.drawable.profile_img).circleCrop().into(profileimg);
         Glide.with(this).load(R.drawable.profile_img).circleCrop().into(profileimg_like);
         Glide.with(this).load(R.drawable.profile_img).circleCrop().into(profileimg_reply);
+
+        //Create New Feed
+        imgCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getContext(),subActivity_Feed_Create.class);
+                startActivity(intent);
+            }
+        });
+
 
 
         // Inflate the layout for this fragment
