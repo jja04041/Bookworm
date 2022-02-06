15129b31159a134c73273cb373faa10ab67d2fff
jja04041 <96465763+jja04041@users.noreply.github.com>
2022-02-06 22:28:11 +0900

    2월 6일 시작

---
+++ app/src/main/java/com/example/bookworm/Challenge/activity_info_challenge.java
@@ -0,0 +1,16 @@
+package com.example.bookworm.Challenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bookworm.R;
+
+public class activity_info_challenge extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_challenge);
+    }
+}
