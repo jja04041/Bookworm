ae6d6e78083f8f247e1d1c75532015f57ac058b6
Durumyisking <80432748+Durumyisking@users.noreply.github.com>
2022-02-08 17:21:11 +0900

    구글로그인 임시커밋

--- app/src/main/java/com/example/bookworm/Login/activity_login.java
+++ app/src/main/java/com/example/bookworm/Login/activity_login.java
@@ -7,13 +7,26 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.example.bookworm.MainActivity;
 import com.example.bookworm.R;
 import com.example.bookworm.User.UserInfo;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.kakao.auth.AuthType;
@@ -27,6 +40,9 @@ public class activity_login extends Activity {
     private FirebaseAuth mAuth;
     private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
     private DatabaseReference databaseReference = firebaseDatabase.getReference();
+    private GoogleSignInClient mGoogleSignInClient;
+
+    private int RC_SIGN_IN=123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +53,30 @@ public class activity_login extends Activity {
 
         Session session = Session.getCurrentSession();
         session.addCallback(new SessionCallback());
+
+
+        // 구글
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.firebase_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        ImageButton google_login_button = (ImageButton) findViewById(R.id.btn_login_google);
+
+        google_login_button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                signIn();
+            }
+        });
+
+
+        // 카카오
 
         if (Session.getCurrentSession().checkAndImplicitOpen()) {
             Log.d(TAG, "onClick: 로그인 세션살아있음");
@@ -54,8 +94,6 @@ public class activity_login extends Activity {
                     session.open(AuthType.KAKAO_LOGIN_ALL, activity_login.this);
             }
         });
-
-        ImageButton google_login_button = (ImageButton) findViewById(R.id.btn_login_google);
     }
 
     @Override
@@ -66,6 +104,12 @@ public class activity_login extends Activity {
     }
 
     @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         // 카카오톡|스토리 간편로그인 실행 결과를 받아서 SDK로 전달
         if (Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
@@ -73,6 +117,19 @@ public class activity_login extends Activity {
         }
 
         super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // 구글 로그인 성공시 파이어베이스 연동
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // 구글 로그인 실패
+                Log.w(TAG, "Google sign in failed", e);
+                Toast.makeText(getApplicationContext(), "Google sign in Failed", Toast.LENGTH_LONG).show();
+            }
+        }
     }
     public void move(UserInfo UserInfo){
         Intent intent=new Intent(this,MainActivity.class);
@@ -94,7 +151,66 @@ public class activity_login extends Activity {
                 } else {
                      Log.d("fucntion signup", "nono token ");
                 }
-        }
+    }
+
+
+
+
+
+
+    // 구글 관련 메소드
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(getApplicationContext(), "Complete", Toast.LENGTH_LONG).show();
+
+                        } else {
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(getApplicationContext(), "Authentication Failed", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void signOut() {
+        mAuth.signOut();
+
+        mGoogleSignInClient.signOut().addOnCompleteListener(this,
+                new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(getApplicationContext(), "Complete", Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
+
+    private void revokeAccess() {
+        mAuth.signOut();
+
+        mGoogleSignInClient.revokeAccess().addOnCompleteListener(this,
+                new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(getApplicationContext(), "Complete", Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
+
 }
 
 
