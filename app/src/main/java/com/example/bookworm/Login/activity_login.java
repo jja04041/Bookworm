57224111d88f4a00fcac34c506b96fd1e66d9e91
Durumyisking <80432748+Durumyisking@users.noreply.github.com>
2022-02-17 23:07:16 +0900

    shared preference 전 백업

--- app/src/main/java/com/example/bookworm/Login/activity_login.java
+++ app/src/main/java/com/example/bookworm/Login/activity_login.java
@@ -3,14 +3,12 @@ package com.example.bookworm.Login;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.example.bookworm.MainActivity;
@@ -22,15 +20,8 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.kakao.auth.AuthType;
 import com.kakao.auth.Session;
 
@@ -141,6 +132,14 @@ public class activity_login extends Activity {
         Intent intent = new Intent(this, MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
         intent.putExtra("userinfo",UserInfo);
+
+        SharedPreferences pref = getSharedPreferences("user",MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+
+        editor.putString("name", UserInfo.username);
+        editor.putString("email", UserInfo.email);
+        editor.commit();
+
         startActivity(intent);
         this.finish();
     }
