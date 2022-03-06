36840faf5f98da88812bcf7e10c441df359d6206
jja04041 <96465763+jja04041@users.noreply.github.com>
2022-03-07 01:41:20 +0900

    3/7 머지하기 전

--- app/src/main/java/com/example/bookworm/Challenge/subActivity/subactivity_challenge_challengeinfo.java
+++ app/src/main/java/com/example/bookworm/Challenge/subActivity/subactivity_challenge_challengeinfo.java
@@ -10,11 +10,14 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.bookworm.Challenge.items.Challenge;
 import com.example.bookworm.R;
 import com.example.bookworm.Search.items.Book;
 import com.example.bookworm.User.UserInfo;
 import com.example.bookworm.modules.FBModule;
+
+import java.util.ArrayList;
 
 public class subactivity_challenge_challengeinfo extends AppCompatActivity {
 
@@ -30,21 +33,34 @@ public class subactivity_challenge_challengeinfo extends AppCompatActivity {
         setContentView(R.layout.subactivity_challenge_challengeinfo);
 
         btn_back = findViewById(R.id.btnBack);
-        tv_bookname = findViewById(R.id.tv_challengeinfo_bookname); // 책 제목
+        btn_join = findViewById(R.id.btn_challenge_join); // 챌린지 참여 버튼
+
+        Thumbnail = findViewById(R.id.ivThumbnail); // 썸네일
+
         tv_challenge_end = findViewById(R.id.tv_challenge_end); // ~~~~에 종료됨
         tv_Dday = findViewById(R.id.Dday); // #일 남음
         tv_challenge_current = findViewById(R.id.tv_Current_participants); // 현재 참여자 #/#
         tv_challenge_max = findViewById(R.id.tv_max_participants); // 최대 참여자 #/#
+        tv_bookname = findViewById(R.id.tv_challengeinfo_bookname); // 책 제목
         tv_end_date = findViewById(R.id.tv_challengeinfo_end); // 마감일
+        tv_current_participants = findViewById(R.id.tv_challengeinfo_Current_participants); // 참여자 ##명
         tv_creator = findViewById(R.id.tv_challengeinfo_creator); // 생성자 닉네임
-        btn_join = findViewById(R.id.btn_challenge_join); // 챌린지 참여 버튼
+
         board = findViewById(R.id.FL_board); // 인증게시판
-        tv_current_participants = findViewById(R.id.tv_Current_participants); // 참여자 ##명
 
-        Thumbnail = findViewById(R.id.ivThumbnail); // 썸네일
-
+        //넘겨받은 값 챌린지 객체에 넣음
         Intent intent = getIntent();
         challenge = (Challenge) intent.getSerializableExtra("challengeInfo");
+
+        Glide.with(this).load(challenge.getBookThumb()).into(Thumbnail);
+
+        tv_challenge_end.setText(challenge.getEndDate());
+        //tv_Dday.setText();
+        tv_challenge_current.setText(String.valueOf(challenge.getCurrentPart().size())); // 받아온 ArrayList 의 길이를 넣음 (현재 참여 인원 수 )
+        tv_challenge_max.setText(String.valueOf(challenge.getMaxPart()));
+        tv_end_date.setText(challenge.getEndDate());
+        tv_current_participants.setText(String.valueOf(challenge.getCurrentPart().size()));
+        tv_creator.setText(challenge.getMaster());
 
         tv_bookname.setText(challenge.getBookTitle());
 
