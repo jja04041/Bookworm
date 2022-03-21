6aaa830448abb678a5b8fd6322e4ab9c9e08e3eb
jja04041 <96465763+jja04041@users.noreply.github.com>
2022-03-22 03:11:32 +0900

    라벨 기능 구현

--- app/src/main/java/com/example/bookworm/Feed/subActivity_Feed_Create.java
+++ app/src/main/java/com/example/bookworm/Feed/subActivity_Feed_Create.java
@@ -1,9 +1,11 @@
 package com.example.bookworm.Feed;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
@@ -17,6 +19,7 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.os.Parcelable;
 import android.provider.MediaStore;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
@@ -113,8 +116,19 @@ public class subActivity_Feed_Create extends AppCompatActivity {
         imgProfile = findViewById(R.id.ivProfileImage);
         tvFinish = findViewById(R.id.tvFinish);
         tvNickName = findViewById(R.id.tvNickname);
-        tvlabel1 = findViewById(R.id.tvlabel1);
+//        tvlabel1 = findViewById(R.id.tvlabel1);
         LLlabel = findViewById(R.id.llLabel);
+
+        //피드 생성화면에 존재하는 라벨
+        TextView feedCreateLabel[] = new TextView[5];
+        int[] feedCreateLabelID = {R.id.tvlabel1, R.id.tvlabel2, R.id.tvlabel3, R.id.tvlabel4, R.id.tvlabel5,};
+
+        //우선 빈 껍데기만 있는 라벨을 보이지 않게 설정해놓음
+        for (int i = 0; i < feedCreateLabel.length; i++) {
+            feedCreateLabel[i] = findViewById(feedCreateLabelID[i]);
+            feedCreateLabel[i].setVisibility(View.INVISIBLE);
+        }
+
 
         //tvlabel1.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#55ff0000"))); //자바로 BackgroundTint 설정
 
@@ -134,12 +148,18 @@ public class subActivity_Feed_Create extends AppCompatActivity {
         customDialog.requestWindowFeature(Window.FEATURE_NO_TITLE); // 타이틀 제거
         customDialog.setContentView(R.layout.custom_dialog_label);
 
-        LLlabel.setOnClickListener(new View.OnClickListener() {
+        binding.addLabel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 showcustomDialog();
             }
         });
+
+//        LLlabel.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//            }
+//        });
 
 
 //        btnAdd.setOnClickListener(new View.OnClickListener() {
@@ -228,23 +248,132 @@ public class subActivity_Feed_Create extends AppCompatActivity {
     }
 
     // 커스텀 다이얼로그(라벨)를 디자인하는 함수
-    public void showcustomDialog () {
+    public void showcustomDialog() {
         customDialog.show(); // 다이얼로그 띄우기
 
-        Button noBtn = customDialog.findViewById(R.id.btnCancle);
-        noBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // 원하는 기능 구현
-                customDialog.dismiss(); // 다이얼로그 닫기
-            }
-        });
-        // 네 버튼
+        //감정에 해당하는 라벨
+        TextView Emotion[] = new TextView[7];
+        int[] EmotionID = {R.id.tvEmotion0, R.id.tvEmotion1, R.id.tvEmotion2, R.id.tvEmotion3, R.id.tvEmotion4, R.id.tvEmotion5, R.id.tvEmotion6,};
+
+        //추천에 해당하는 라벨
+        TextView Recommend[] = new TextView[5];
+        int[] RecommendID = {R.id.tvRecommend0, R.id.tvRecommend1, R.id.tvRecommend2, R.id.tvRecommend3, R.id.tvRecommend4};
+
+        //피드 생성화면에 존재하는 라벨
+        TextView feedCreateLabel[] = new TextView[5];
+        int[] feedCreateLabelID = {R.id.tvlabel1, R.id.tvlabel2, R.id.tvlabel3, R.id.tvlabel4, R.id.tvlabel5,};
+
+        //피드 생성화면의 라벨 findViewById 연결
+        for (int i = 0; i < feedCreateLabel.length; i++) {
+            feedCreateLabel[i] = findViewById(feedCreateLabelID[i]);
+        }
+
+        ArrayList<String> label = new ArrayList<String>(); //선택한 라벨 목록을 담을 리스트
+
+
+        //감정 라벨 선택시 선택되면서 배경색 변경
+        for (int i = 0; i < Emotion.length; i++) {
+            final int index = i;
+            Emotion[index] = customDialog.findViewById(EmotionID[index]);
+            Emotion[index].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (Emotion[index].isSelected()) {
+                        Emotion[index].setSelected(false);
+                    } else {
+                        Emotion[index].setSelected(true);
+                    }
+                }
+            });
+        }
+
+        //추천 라벨 선택시 선택되면서 배경색 변경
+        for (int i = 0; i < Recommend.length; i++) {
+            final int index = i;
+            Recommend[index] = customDialog.findViewById(RecommendID[index]);
+            Recommend[index].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (Recommend[index].isSelected()) {
+                        Recommend[index].setSelected(false);
+                    } else {
+                        Recommend[index].setSelected(true);
+                    }
+                }
+            });
+        }
+
+
+        // 완료 버튼
         customDialog.findViewById(R.id.btnOk).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // 원하는 기능 구현
-                customDialog.dismiss(); // 다이얼로그 닫기
+
+                TextView tvlabelHint = findViewById(R.id.tvlabelHint); //라벨을 추가해보세요 라는 문구
+                int labelCount = 0; //라벨은 최대 5개까지만 설정하게끔 작동하게 하는 변수
+
+                //라벨 리스트 초기화하고 기존의 라벨 칸마다 INVISIBLE 시키고 Text를 Null로 만듦
+                label.clear();
+//                for (int i = 0; i < 5; i++) {
+//                    feedCreateLabel[i].setVisibility(View.INVISIBLE);
+//                    feedCreateLabel[i].setText(null);
+//                }
+
+                //감정 라벨이 선택돼있으면 라벨 리스트에 선택된 항목 추가
+                for (int i = 0; i < Emotion.length; i++) {
+                    if (Emotion[i].isSelected()) {
+                        label.add(Emotion[i].getText().toString());
+                        labelCount += 1;
+                    }
+                }
+
+                //추천 라벨이 선택돼있으면 라벨 리스트에 선택된 항목 추가
+                for (int i = 0; i < Recommend.length; i++) {
+                    if (Recommend[i].isSelected()) {
+                        label.add(Recommend[i].getText().toString());
+                        labelCount += 1;
+                    }
+                }
+
+                //선택된 라벨의 갯수가 5개 이하라면 피드 작성 화면의 라벨을 선택한 라벨로 채워넣고 VISIBLE 시킴
+                if (labelCount <= 5) {
+                    for (int i = 0; i < labelCount; i++) {
+                        feedCreateLabel[i].setVisibility(View.VISIBLE);
+                        feedCreateLabel[i].setText(label.get(i));
+                    }
+                    for (int i = 4; i >= labelCount; i--) {
+                        feedCreateLabel[i].setVisibility(View.INVISIBLE);
+                        feedCreateLabel[i].setText(null);
+                    }
+                    customDialog.dismiss(); // 다이얼로그 닫기
+
+                    //라벨이 없으면 버튼의 텍스트를 "라벨 추가", 하나라도 있으면 "라벨 수정" 으로 설정
+                    if (TextUtils.isEmpty(feedCreateLabel[0].getText())) {
+                        binding.addLabel.setText("라벨 추가");
+                    } else {
+                        binding.addLabel.setText("라벨 수정");
+                    }
+
+                } else {
+                    //라벨을 6개 이상 선택했다면 AlertDialog를 띄워줌
+                    new AlertDialog.Builder(current_context)
+                            .setMessage("라벨은 최대 5개까지 선택이 가능합니다.")
+                            .setPositiveButton("네", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            }).show();
+                }
+
+                //라벨을 1개 이상 5개 이하로 선택했다면 "라벨을 추가해보세요" 라는 문구를 안보이게 함.
+                if (labelCount > 0 && labelCount <= 5) {
+                    tvlabelHint.setVisibility(View.INVISIBLE);
+                } else if (labelCount == 0) { //라벨을 하나도 선택하지 않았다면 다시 문구를보이게 함. 6개 이상 선택했다면 아무런 작동 없음 (이전의 상태를 따라감)
+                    tvlabelHint.setVisibility(View.VISIBLE);
+                }
+
             }
         });
     }
