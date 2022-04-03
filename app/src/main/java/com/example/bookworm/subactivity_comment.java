d3c14e6a3997cb58d49d211f285860defce5bd11
jja04041 <96465763+jja04041@users.noreply.github.com>
2022-04-03 09:35:58 +0900

    4/3 댓글창 액티비티 일단 임시로...

---
+++ app/src/main/java/com/example/bookworm/subactivity_comment.java
@@ -0,0 +1,14 @@
+package com.example.bookworm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class subactivity_comment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.subactivity_comment);
+    }
+}
