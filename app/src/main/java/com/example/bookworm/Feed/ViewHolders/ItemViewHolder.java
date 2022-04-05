7c8883736022d26f352c16b7bf6ff29b3176ecf8
jja04041 <96465763+jja04041@users.noreply.github.com>
2022-04-05 18:25:29 +0900

    4/5

--- app/src/main/java/com/example/bookworm/Feed/ViewHolders/ItemViewHolder.java
+++ app/src/main/java/com/example/bookworm/Feed/ViewHolders/ItemViewHolder.java
@@ -39,7 +39,6 @@ public class ItemViewHolder extends RecyclerView.ViewHolder {
     int limit = 0;
     Boolean restricted = false;
     Context context;
-    customDialog dialog;
 
     //생성자를 만든다.
     public ItemViewHolder(@NonNull View itemView, Context context) {
