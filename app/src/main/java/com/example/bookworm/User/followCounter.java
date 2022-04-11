01f0d3b7bf36b88f0fc031aae9f211a80c50841e
jja04041 <96465763+jja04041@users.noreply.github.com>
2022-04-12 00:38:59 +0900

    4/11 팔로우 카운트 작동

--- app/src/main/java/com/example/bookworm/User/followCounter.java
+++ app/src/main/java/com/example/bookworm/User/followCounter.java
@@ -67,8 +67,8 @@ public class followCounter {
         db.runTransaction(new Transaction.Function<Void>() {
             @Override
             public Void apply(@NonNull Transaction transaction) throws FirebaseFirestoreException {
-                transaction.update(ref, "followerCounts", FieldValue.increment(count));
-                transaction.update(Myref, "followingCounts", FieldValue.increment(count));
+                transaction.update(ref, "UserInfo.followerCounts", FieldValue.increment(count));
+                transaction.update(Myref, "UserInfo.followingCounts", FieldValue.increment(count));
                 if (count == 1) {
                     transaction.set(refFollower, nowuserInfo);
                     transaction.set(refFollowing, userInfo);
