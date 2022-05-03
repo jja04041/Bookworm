c07683ea443ccd61d90a606199102f5268bf41f8
jja04041 <96465763+jja04041@users.noreply.github.com>
2022-05-04 01:08:21 +0900

    5/4 인증글 없을때도 작동하도록 함

--- app/src/main/java/com/example/bookworm/BottomMenu/Challenge/Board/BoardFB.java
+++ app/src/main/java/com/example/bookworm/BottomMenu/Challenge/Board/BoardFB.java
@@ -33,7 +33,11 @@ public class BoardFB {
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if (task.isSuccessful()) {
                     QuerySnapshot querySnapshot = task.getResult();
-                    ((subactivity_challenge_board) context).moduleUpdated(querySnapshot.getDocuments());
+                    if (!querySnapshot.isEmpty()) {//가져온 데이터가 존재할경우
+                        ((subactivity_challenge_board) context).moduleUpdated(querySnapshot.getDocuments());
+                    }else {//가져온 데이터가 존재하지 않을 경우
+                        ((subactivity_challenge_board) context).isEmptyBoard(true);//인증글이 없습니다 라는 문구를 띄워줌
+                    }
                 }
             }
         });
