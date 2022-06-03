40d1c2c841d66f342f92c1c17d63d226fc8f3620
jja04041 <96465763+jja04041@users.noreply.github.com>
2022-06-03 20:21:18 +0900

    6/3 - 자기소개 수정 기능 구현 - 수정하고 화면 두번 왔다갔다 하기 전까진 이전 자기소개가 출력됨... 수정 필요

--- app/src/main/java/com/example/bookworm/bottomMenu/profile/fragment_profile.java
+++ app/src/main/java/com/example/bookworm/bottomMenu/profile/fragment_profile.java
@@ -1,12 +1,15 @@
 package com.example.bookworm.bottomMenu.profile;
 
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LifecycleObserver;
@@ -23,6 +26,8 @@ import com.example.bookworm.extension.follow.view.FollowViewModelImpl;
 import com.example.bookworm.extension.follow.view.FollowerActivity;
 import com.example.bookworm.core.internet.FBModule;
 
+import java.util.HashMap;
+
 public class fragment_profile extends Fragment implements LifecycleObserver {
 
     private BookWorm bookworm;
@@ -30,6 +35,7 @@ public class fragment_profile extends Fragment implements LifecycleObserver {
     private FragmentProfileBinding binding;
     private Context current_context;
     private FBModule fbModule;
+    private profileFB profileFB;
     UserInfoViewModel pv;
     FollowViewModelImpl fv;
     SubMenuPagerAdapter menuPagerAdapter;
@@ -43,6 +49,7 @@ public class fragment_profile extends Fragment implements LifecycleObserver {
         pv = new ViewModelProvider(this, new UserInfoViewModel.Factory(current_context)).get(UserInfoViewModel.class);
         fv = new ViewModelProvider(this, new FollowViewModelImpl.Factory(current_context)).get(FollowViewModelImpl.class);
         fbModule = new FBModule(current_context);
+        profileFB = new profileFB(current_context);
         //서브 메뉴를 보여주기위한 어댑터
         menuPagerAdapter = new SubMenuPagerAdapter(getChildFragmentManager());
         binding.subMenuViewPager.setAdapter(menuPagerAdapter);
@@ -65,6 +72,7 @@ public class fragment_profile extends Fragment implements LifecycleObserver {
             achievement = new Achievement(current_context, fbModule, userinfo, bookworm);
             binding.tvFollowerCount.setText(String.valueOf(userinfo.getFollowerCounts()));
             binding.tvFollowingCount.setText(String.valueOf(userinfo.getFollowingCounts()));
+
             setUI(userinfo);
         });
         fv.getData().observe(getViewLifecycleOwner(), userInfo -> {
@@ -83,6 +91,7 @@ public class fragment_profile extends Fragment implements LifecycleObserver {
     private void setUI(UserInfo user) {
         Glide.with(this).load(user.getProfileimg()).circleCrop().into(binding.imgFragProfileProfile); //프로필사진 로딩후 삽입.
         binding.tvUserName.setText(user.getUsername());
+        binding.edtIntroduce.setText(user.getIntroduce());
 
         //팔로워액티비티 실행하기
         binding.btnFollower.setOnClickListener(view -> {
@@ -98,6 +107,65 @@ public class fragment_profile extends Fragment implements LifecycleObserver {
             intent.putExtra("token", user.getToken());
             intent.putExtra("page", 1);
             startActivity(intent);
+        });
+
+        binding.btnIntroModify.setOnClickListener(new View.OnClickListener() { //자기소개 수정
+            @Override
+            public void onClick(View view) {
+                if (binding.btnIntroModify.getText().toString().equals("수정")){ //수정하기 버튼을 눌렀을 때
+                    binding.btnIntroModify.setText("완료");
+                    binding.edtIntroduce.setBackgroundColor(Color.parseColor("#2200ff00")); //배경색 변경
+                    binding.edtIntroduce.setEnabled(true); //EditText를 수정 가능한 상태로 만듦
+                    binding.btnModifyCancle.setVisibility(View.VISIBLE); //수정 취소 버튼 노출
+
+                    //키보드 띄우기
+                    InputMethodManager imm = (InputMethodManager) current_context.getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
+                    binding.edtIntroduce.requestFocus(); //초점 이동
+                    binding.edtIntroduce.setSelection(binding.edtIntroduce.length()); //가장 마지막으로 커서 이동
+                } else { //완료 버튼을 눌렀을 때
+                    new AlertDialog.Builder(current_context) //변경하
+                        .setMessage("변경하시겠습니까?")
+                        .setPositiveButton("네", (dialog, which) -> {
+
+                            HashMap<String, Object> map = new HashMap<>();
+                            user.setIntroduce(binding.edtIntroduce.getText().toString()); //userinfo의 자기소개를 변경
+                            map.put("UserInfo.introduce", user.getIntroduce()); //map 객체에 수정할 항목 삽입
+
+                            profileFB.modifyIntroduce(map, user.getToken());
+                            binding.btnIntroModify.setText("수정"); //완료 버튼에서 다시 수정 버튼으로 변경
+                            binding.edtIntroduce.setBackgroundColor(Color.WHITE); //배경색 변경
+                            binding.edtIntroduce.setEnabled(false); //수정 불가능하게 변경
+                            binding.btnModifyCancle.setVisibility(View.INVISIBLE); //수정 취소 버튼 숨김
+
+                            //키보드 내리기
+                            InputMethodManager imm = (InputMethodManager) current_context.getSystemService(Context.INPUT_METHOD_SERVICE);
+                            imm.hideSoftInputFromWindow(binding.edtIntroduce.getWindowToken(), 0);
+                            binding.edtIntroduce.clearFocus(); //초점 제거
+                            dialog.dismiss();
+
+                        })
+                        .setNegativeButton("아니요", (dialog, which)
+                                -> dialog.dismiss())
+                        .show();
+                }
+            }
+        });
+
+        binding.btnModifyCancle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.btnIntroModify.setText("수정"); //완료 버튼에서 다시 수정 버튼으로 변경
+                binding.edtIntroduce.setBackgroundColor(Color.WHITE); //배경색 변경
+                binding.edtIntroduce.setEnabled(false); //수정 불가능하게 변경
+                binding.edtIntroduce.setText(user.getIntroduce()); //기존의 자기소개로 변경
+                binding.btnModifyCancle.setVisibility(View.INVISIBLE); //수정 취소 버튼 숨김
+
+                //키보드 내리기
+                InputMethodManager imm = (InputMethodManager) current_context.getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(binding.edtIntroduce.getWindowToken(), 0);
+                binding.edtIntroduce.clearFocus(); //초점 제거
+            }
         });
 
     }
---
+++ app/src/main/java/com/example/bookworm/bottomMenu/profile/profileFB.java
@@ -0,0 +1,43 @@
+package com.example.bookworm.bottomMenu.profile;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Map;
+
+//fragment_profile에서 자기소개를 수정할때 사용하는 모듈
+public class profileFB {
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    Context context;
+    Task task = null;
+    CollectionReference collectionReference;
+
+
+    profileFB(Context context) {
+        this.context = context;
+    }
+
+    public void modifyIntroduce(Map map, String UserToken) {//token = 유저 토큰
+        collectionReference = db.collection("users");
+        task = collectionReference.document(UserToken).update(map);
+        task.addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
+    }
+
+}
