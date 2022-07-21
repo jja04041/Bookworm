1260f811428ba040e4f4e2464a96add49765a45c
이규연 <lmy6268@gmail.com>
2022-07-21 18:28:44 +0900

    7.21 커밋 목록 / 작성자: 이규연 ---------------------------------------- * 추가 ---------------------------------------- * 삭제 ---------------------------------------- * 수정 - 머지완료 및 메달 범위 수정(== 에서 >= 로) ----------------------------------------

--- app/src/main/java/com/example/bookworm/achievement/Achievement.java
+++ app/src/main/java/com/example/bookworm/achievement/Achievement.java
@@ -2,16 +2,17 @@ package com.example.bookworm.achievement;
 
 import android.content.Context;
 
-import com.example.bookworm.R;
+
 import com.example.bookworm.bottomMenu.bookworm.BookWorm;
 import com.example.bookworm.bottomMenu.profile.UserInfoViewModel;
 import com.example.bookworm.core.internet.FBModule;
+import com.example.bookworm.core.login.GlobalApplication;
 import com.example.bookworm.core.userdata.PersonalD;
 import com.example.bookworm.core.userdata.UserInfo;
 
 import java.util.HashMap;
 
-public class Achievement  {
+public class Achievement {
 
     Context context;
     FBModule fbModule;
@@ -50,7 +51,7 @@ public class Achievement  {
             map.put("bookworm_wormvec", bookworm.getWormvec());
         }
         // 배경 보상
-        else if(type == 1) {
+        else if (type == 1) {
 //            bookworm.getBgvec().add(_drawblepath);
             bookworm.getAchievementmap().put(_key, true);
             map.put("bookworm_achievementmap", bookworm.getAchievementmap());
@@ -83,7 +84,7 @@ public class Achievement  {
 
         HashMap<String, Object> map = new HashMap<>();
 
-        if(userinfo.getGenre().get("자기계발") != null) {
+        if (userinfo.getGenre().get("자기계발") != null) {
             if (userinfo.getGenre().get("자기계발") == 2) {
                 if (null == bookworm.getAchievementmap().get("자존감왕")) {
                     ExecuteFB("confidence", "자존감왕", 0);
@@ -91,21 +92,21 @@ public class Achievement  {
             }
         }
 
-        if(userinfo.getGenre().get("소설") != null) {
+        if (userinfo.getGenre().get("소설") != null) {
             if (userinfo.getGenre().get("소설") == 1) {
                 if (null == bookworm.getAchievementmap().get("가을볼레")) {
                     ExecuteFB("fall", "가을볼레", 0);
                 }
             }
         }
-        if(userinfo.getGenre().get("육아") != null) {
+        if (userinfo.getGenre().get("육아") != null) {
             if (userinfo.getGenre().get("육아") == 3) {
                 if (null == bookworm.getAchievementmap().get("좋은엄마")) {
                     ExecuteFB("mom", "좋은엄마", 0);
                 }
             }
         }
-        if(userinfo.getGenre().get("어린이") != null) {
+        if (userinfo.getGenre().get("어린이") != null) {
             if (userinfo.getGenre().get("어린이") == 3) {
                 if (null == bookworm.getAchievementmap().get("착한어린이")) {
                     ExecuteFB("child", "착한어린이", 0);
@@ -113,38 +114,38 @@ public class Achievement  {
             }
         }
 
-        if(userinfo.getGenre().get("청소년") != null) {
+        if (userinfo.getGenre().get("청소년") != null) {
             if (userinfo.getGenre().get("청소년") == 3) {
-                    if(null == bookworm.getAchievementmap().get("사춘기볼레")) {
-                        ExecuteFB("student", "사춘기볼레", 0);
-                    }
+                if (null == bookworm.getAchievementmap().get("사춘기볼레")) {
+                    ExecuteFB("student", "사춘기볼레", 0);
                 }
+            }
         }
 
-        if(userinfo.getGenre().get("공부") != null) {
+        if (userinfo.getGenre().get("공부") != null) {
             if (userinfo.getGenre().get("공부") == 3) {
-                if(null == bookworm.getAchievementmap().get("공부벌레")) {
+                if (null == bookworm.getAchievementmap().get("공부벌레")) {
                     ExecuteFB("study", "공부벌레", 0);
                 }
             }
         }
-        if(userinfo.getGenre().get("사회") != null) {
+        if (userinfo.getGenre().get("사회") != null) {
             if (userinfo.getGenre().get("사회") == 3) {
-                if(null == bookworm.getAchievementmap().get("사회왕")) {
+                if (null == bookworm.getAchievementmap().get("사회왕")) {
                     ExecuteFB("social", "사회왕", 0);
                 }
             }
         }
-        if(userinfo.getGenre().get("과학") != null) {
+        if (userinfo.getGenre().get("과학") != null) {
             if (userinfo.getGenre().get("과학") == 3) {
-                if(null == bookworm.getAchievementmap().get("싸이언쓰볼레")) {
+                if (null == bookworm.getAchievementmap().get("싸이언쓰볼레")) {
                     ExecuteFB("science", "싸이언쓰볼레", 0);
                 }
             }
         }
-        if(userinfo.getGenre().get("인문") != null) {
+        if (userinfo.getGenre().get("인문") != null) {
             if (userinfo.getGenre().get("인문") == 3) {
-                if(null == bookworm.getAchievementmap().get("문학볼레")) {
+                if (null == bookworm.getAchievementmap().get("문학볼레")) {
                     ExecuteFB("literature", "문학볼레", 0);
                 }
             }
@@ -156,9 +157,9 @@ public class Achievement  {
 //                }
 //            }
 //        }
-        if(userinfo.getGenre().get("만화") != null) {
+        if (userinfo.getGenre().get("만화") != null) {
             if (userinfo.getGenre().get("만화") == 3) {
-                if(null == bookworm.getAchievementmap().get("만화광")) {
+                if (null == bookworm.getAchievementmap().get("만화광")) {
                     ExecuteFB("cartoon", "만화광", 0);
                 }
             }
@@ -169,20 +170,20 @@ public class Achievement  {
 
 
         if (userinfo.getLikedPost().size() == 2) {
-                if(null == bookworm.getAchievementmap().get("하트배경")) {
-                    ExecuteFB("bg_heart", "하트배경", 1);
-                }
+            if (null == bookworm.getAchievementmap().get("하트배경")) {
+                ExecuteFB("bg_heart", "하트배경", 1);
+            }
         }
 
 
         //인증글 업적
         //티어는 1 ~ n까지로, 일단 1티어는 브론즈로 해둠
 
-        if (userinfo.getCompletedChallenge() == 3 && userinfo.getTier() < 1) {
+        if (userinfo.getCompletedChallenge() >= 3 && userinfo.getTier() < 1) {
             userinfo.setTier(Long.parseLong(String.valueOf(1)));
-            ExecuteFB(1, "브론즈 티어", 2); //1티어, 브론즈 티어, type(인증글인지, 볼레인지)
+            ExecuteFB("1", "브론즈 티어", 2); //1티어, 브론즈 티어, type(인증글인지, 볼레인지)
         }
 
 
-     }
+    }
 }
