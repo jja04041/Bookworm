483c548dea6ba29b4892634b03ca2580b22d1516
이규연 <lmy6268@gmail.com>
2022-07-27 17:36:14 +0900

    7.24 커밋 목록 / 작성자: 이규연 ---------------------------------------- * 추가 ---------------------------------------- * 삭제 ---------------------------------------- * 수정 - creater 변수를 AlbumData 객체에 추가하여, 앨범 데이터를 불러올 때 작성자 정보도 불러올 수 있도록함 - 앨범을 보는 액티비티(현재의 ShowAlbumContentActivity.kt)를 사용자가 앨범을 생성하거나 편집할 때도 해당 액티비티를 재사용할 수 있도록 한다. ----------------------------------------

--- app/src/main/java/com/example/bookworm/bottomMenu/profile/submenu/album/AlbumData.kt
+++ app/src/main/java/com/example/bookworm/bottomMenu/profile/submenu/album/AlbumData.kt
@@ -10,12 +10,15 @@ class AlbumData : Serializable {
     var albumName: String? = null //앨범 이름
     var containsList= ArrayList<Feed>() //앨범이 가진 피드 리스트
     var albumId: String? = null
+    var creater: String?= null
 
     //파이어베이스에서 전달받은 데이터를 객체에 담음
     fun addData(data: Map<String, Any>) {
         thumbnail = data["thumbnail"] as String?
         albumName = data["albumName"] as String?
         albumId = data["albumId"] as String?
+        if("creater" in data) creater = data["creater"] as String?
+
         for(i in data["containsList"] as ArrayList<HashMap<*,*>>){
             var item = Feed()
             item.setFeedData(i)
